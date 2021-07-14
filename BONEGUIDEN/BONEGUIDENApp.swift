0df529f42d3f418e63ee446fc412d3135dddd1f2
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2021-07-14 17:52:38 +0200

    Initial Commit

---
+++ BONEGUIDEN/BONEGUIDENApp.swift
@@ -0,0 +1,17 @@
+//
+//  BONEGUIDENApp.swift
+//  BONEGUIDEN
+//
+//  Created by Amina Amin on 2021-07-14.
+//
+
+import SwiftUI
+
+@main
+struct BONEGUIDENApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BONEGUIDEN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BONEGUIDEN
+//
+//  Created by Amina Amin on 2021-07-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
